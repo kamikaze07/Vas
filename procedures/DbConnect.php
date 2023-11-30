22317b925e07fab6efe426784de988e7c1486075
kamikaze07 <cesarkun07@gmail.com>
2023-11-30 15:50:43 -0600

    DBConnect

---
+++ procedures/DbConnect.php
@@ -0,0 +1,16 @@
+<?php
+    date_default_timezone_set('America/Mexico_City');
+    $servername = "localhost";
+    $username = "admin";
+    $password = "Vas2023.";
+    $dbname = "vas";
+    $port = "3306";
+    
+    // Create connection
+    $conexion= mysqli_connect($servername,$username,$password,$dbname,$port);
+    // Check connection
+    if (!$conexion) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+    
+?>
